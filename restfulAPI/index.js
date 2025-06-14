111b4abf81dd4cbf2ac7fd73200c4ec43a016cbe
SharatRokkam <rokkamsharath1@gmail.com>
2025-06-14 13:33:03 +0530

    restapi

--- restfulAPI/index.js
+++ restfulAPI/index.js
@@ -1,16 +1,17 @@
 import express from "express";
 import data from "./MOCK_DATA.json" with {type :"json"};
 // const express = require("express");
-// const users = require("./MOCK_DATA.json");
+// const data = require("./MOCK_DATA.json");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(express.json());
+
 
 // app.get("/users", (req, res) => {
 //   return res.send(data);
 // });
-
 
 // REST API - GET, POST, PATCH, DELETE
 //Get all the users
@@ -18,32 +19,31 @@ app.get("/api/users", (req, res) => {
   return res.json(data);
 });
 
-
 //get single user and can update and delete
-app.route("/api/users/:id")
-.get( (req, res) =>{
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
     const id = Number(req.params.id);
-    console.log(id)
+    console.log(id);
 
-   const user =  data.find((user) => user.id == id)
-   return res.json(user)
-})
-.patch((req, res) =>{
-
+    const user = data.find((user) => user.id == id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
     const id = Number(req.params.id);
-    return res.json({status : "pending"})
-})
-.delete((req, res) =>{
+    return res.json({ status: "pending" });
+  })
+  .delete((req, res) => {
     const id = Number(req.params.id);
-    return res.json({status : "pending"})
-})
-
+    return res.json({ status: "pending" });
+  });
 
 //add new user to the JSON data
-app.post("/api/users", (req, res) =>{
-    return res.json({status : "pending"})
-})
-
+app.post("/api/users", (req, res) => {
+  console.log(req.body);
+  data.push(req.body);
+  return res.json(req.body)
+});
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
